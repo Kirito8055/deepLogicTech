b8dd48d4eda55b40074d87e59d0f48d1406d355b
Dhruv Mangal <104397045+Kirito8055@users.noreply.github.com>
2024-04-05 23:24:57 +0530

    Update DLT_CovidProblem.js

--- DLT_CovidProblem.js
+++ DLT_CovidProblem.js
@@ -1,46 +1,35 @@
 let days = 5;
 let assuranceProbability = 0.98;
-const numberOfSimulations = 500000;
+const numberOfSimulations = 50000;
+let distances = [];
 
-let max = (a, b) => {
-  if (a > b) return a;
-  else return b;
-};
+// function to extrapolate the distance according to distance v/s probability equation
+function distanceTravelledByPerson(probability) {
+  return (38 - 35 * probability) / 2;
+}
 
-function distanceTravelledByPerson(probability){
-  return (38 - 35*probability)/2;
-} 
-
-let maxRadius = 0;
+// many simulations
 for (let i = 0; i < numberOfSimulations; i++) {
   let totalInfected = [0];
-  totalInfected.push(0); // the distance of infected people from origin
 
   // simulation per day
   for (let j = 0; j < days; j++) {
-
-    // distance of newly infected people per day
     let newInfected = [];
-
-    // new infected per person per day
-    totalInfected.forEach((person) => {
+    for (let i = 0; i < totalInfected.length; i++) {
       let probability = Math.random();
-      let dist = distanceTravelledByPerson(probability); 
-
-      // probability of getting infected
-      if (Math.random() <= assuranceProbability) { 
-        newInfected.push(person + dist);
-      }
-    });
-
-    // add the newly infected people per day to the total infected people
+      let dist = distanceTravelledByPerson(probability);
+      totalInfected[i] += dist;
+      newInfected.push(totalInfected[i]);
+    }
     newInfected.forEach((infectedPerson) => {
       totalInfected.push(infectedPerson);
     });
   }
+  let maxRadius = 0;
   totalInfected.forEach((distanceByPerson) => {
-      maxRadius = max(maxRadius, distanceByPerson);
-    });
-  }
-
-console.log(maxRadius);
+    maxRadius = Math.max(maxRadius, distanceByPerson);
+  });
+  distances.push(maxRadius);
+}
+distances.sort();
+console.log(distances[0.02 * numberOfSimulations]/2);
